b705d8a2b79f7f5137710a9bb03e787a4d5e86da
Batrak Yurii <navid7498@gmail.com>
2022-10-24 18:20:29 +0300

    Added base entity model to DA layer. Created context class for DB

---
+++ ClashOfMusic.Api.Data/ClashOfMusicContext.cs
@@ -0,0 +1,62 @@
+﻿using ClashOfMusic.Api.Data.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClashOfMusic.Api.Data
+{
+    public class ClashOfMusicContext : IdentityDbContext<User>
+    {
+        public ClashOfMusicContext(DbContextOptions options) : base(options)
+        {
+            
+
+        }
+
+        public DbSet<PlayList> PlayLists { get; set; }
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<Genre>().HasData(
+                new Genre
+                {
+                    Id = 1,
+                    Title = "Pop"
+                },
+                new Genre
+                {
+                    Id = 2,
+                    Title = "Hip-hop"
+                },new Genre
+                {
+                    Id = 3,
+                    Title = "Trap"
+                },
+                new Genre
+                {
+                    Id = 4,
+                    Title = "Rock"
+                },
+                new Genre
+                {
+                    Id = 5,
+                    Title = "Rhythm and blues"
+                },
+                new Genre
+                {
+                    Id = 6,
+                    Title = "Disco"
+                }
+                );
+        }
+    }
+}
---
+++ ClashOfMusic.Api.Data/Entities/PlayList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClashOfMusic.Api.Data.Entities
+{
+    public class PlayList
+    {
+        public int Id { get; set; }
+        public  string Title { get; set; }
+
+
+    }
+}
